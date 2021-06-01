78961fea0675590074005ff1b2bacf6b70d356c2
SantiagoEche0799 <santiago.eche.duque.99910@gmail.com>
2021-06-01 16:28:55 -0500

    project init

---
+++ src/test/java/co/edu/usbbog/bdd/ApiBancoMedellinApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.usbbog.bdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiBancoMedellinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
